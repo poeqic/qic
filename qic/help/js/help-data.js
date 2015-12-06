687a80a54673007384b46a1fab509023563e9308
thirdy <thirdy.derivera@gmail.com>
2015-12-06 23:32:24 +0800

    inital help htm

---
+++ qic/help/js/help-data.js
@@ -0,0 +1,6 @@
+var dataSet = [
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500" ],
+    [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
+    [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+];
