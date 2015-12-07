48bfc8d6ecc64e2535842c03d44cc7704c63f41f
Manuel Schermuly <prinzeru@gmail.com>
2015-12-07 01:38:25 +0100

    Made the help a bit more pretty

---
+++ qic/help/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	var table = $('#helptable').DataTable( {
+		"sDom": 'flrtip',
+		scrollX: 400,
+		data: dataSet,
+		columns: [
+			{ title: "Term" },
+			{ title: "File" },
+			{ title: "Http Query" }
+		]
+	} );
+
+	$(("<a class='toggle-vis' data-column='2'>Toggle Http Query</a>")).insertBefore('.dataTables_scrollHead');
+
+	$('a.toggle-vis').on( 'click', function (e) {
+		e.preventDefault();
+		// Get the column API object
+		var column = table.column( $(this).attr('data-column') );
+		// Toggle the visibility
+		column.visible( ! column.visible() );
+	} );
+
+
+} );
